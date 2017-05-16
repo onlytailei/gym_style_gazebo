f3536f285a628444b30bb2c44dcf1e03407b2563
Lei Tai <onlytailei@hotmail.com>
2017-05-16 20:03:35 +0200

    need check laser value

--- src/task_env_io.cpp
+++ src/task_env_io.cpp
@@ -14,11 +14,11 @@
 #include "gazebo_env_io.h"
 #include "task_env_io.h"
 
-/// statecallback function 
+/// statecallback function
 template<typename topicType>
 void RL::GetNewTopic<topicType>::StateCallback(const topicType& msg_){
   StateVector.push_back(msg_);
-  if (StateVector.size() > 5) 
+  if (StateVector.size() > 5)
     StateVector.erase(StateVector.begin());
 }
 
@@ -26,7 +26,7 @@ void RL::GetNewTopic<topicType>::StateCallback(const topicType& msg_){
 template<typename topicType>
 RL::GetNewTopic<topicType>::GetNewTopic(ros::NodeHandlePtr rosNode_,
     const std::string topic_name){
-  StateSub = rosNode_->subscribe(topic_name, 1, &GetNewTopic::StateCallback, this);   
+  StateSub = rosNode_->subscribe(topic_name, 1, &GetNewTopic::StateCallback, this);
 }
 
 ////////////////////////////
@@ -34,7 +34,7 @@ RL::TaskEnvIO::TaskEnvIO(
     const std::string service_name,
     const std::string node_name,
     const float sleeping_time):
-  GazeboEnvIO(node_name), 
+  GazeboEnvIO(node_name),
   state_1(new RL::GetNewTopic<RL::STATE_1_TYPE>(this->rosNode, "/camera/depth/image_raw")),
   state_2(new RL::GetNewTopic<RL::STATE_2_TYPE>(this->rosNode, "/gazebo/model_states")),
   laser_scan(new RL::GetNewTopic<sensor_msgs::LaserScanConstPtr>(this->rosNode, "/scan")),
@@ -60,16 +60,20 @@ bool RL::TaskEnvIO::ServiceCallback(
     gym_style_gazebo::PytorchRL::Request &req,
     gym_style_gazebo::PytorchRL::Response &res){
 
+  // TODO the terminal_flag is always false now
+  /*
   if (req.reset){
     while (terminal_flag)
       reset();
   }
-  
+  */
+
   ActionPub.publish(req.action);
   //ros::Duration(sleeping_time_).sleep();
   getRobotState(); //update robot state
   res.reward = rewardCalculate();
   res.terminal = terminal_flag;
+  res.state_1 = *state_1->StateVector.back();
   {
   res.state_2.layout.dim.push_back(std_msgs::MultiArrayDimension());
   res.state_2.layout.dim[0].size = robot_state_.size();
@@ -84,10 +88,13 @@ bool RL::TaskEnvIO::ServiceCallback(
 
 ///////////////////////
 float RL::TaskEnvIO::rewardCalculate(){
+  // TODO check the collision check alwasy return true
+  /*
   if (collision_check()){
     terminal_flag = true;
     return failReward;}
-  else if (terminalCheck()){
+  */
+  if (terminalCheck()){
     terminal_flag = true;
     return terminalReward;}
   else {
@@ -116,10 +123,10 @@ bool RL::TaskEnvIO::target_check(){
 
 ///////////////////////
 bool RL::TaskEnvIO::reset() {
-  // Set a new target for the robot 
+  // Set a new target for the robot
   // Set a new position for the robot
   // Set random position for pedes
-  
+
   getRobotState(); //update robot state
   rewardCalculate(); //check if it is terminal
   return true;
@@ -128,24 +135,24 @@ bool RL::TaskEnvIO::reset() {
 
 ///////////////////////
 void RL::TaskEnvIO::getRobotState(){
-  
+
   gazebo_msgs::ModelStates newStates = state_2->StateVector.back();
-  std::vector<std::string> names = newStates.name; 
-  auto idx_ = std::find(names.begin(), names.end(),"mobile_base")-names.begin(); 
+  std::vector<std::string> names = newStates.name;
+  auto idx_ = std::find(names.begin(), names.end(),"mobile_base")-names.begin();
   assert(idx_ < names.size());
   geometry_msgs::Twist twist_ = newStates.twist.at(idx_);
   geometry_msgs::Pose pose_ = newStates.pose.at(idx_);
 
   //Robot State Update
-  robot_state_.at(0) = atan2(target_pose_.x-pose_.position.x, 
+  robot_state_.at(0) = atan2(target_pose_.x-pose_.position.x,
       target_pose_.y-pose_.position.y);
 
-  robot_state_.at(1) = sqrt(pow((target_pose_.x-pose_.position.x), 2) + 
+  robot_state_.at(1) = sqrt(pow((target_pose_.x-pose_.position.x), 2) +
       pow((target_pose_.y-pose_.position.y), 2));
 
   robot_state_.at(2) = twist_.angular.z;
 
-  robot_state_.at(3) = sqrt(pow(twist_.linear.x, 2) + 
+  robot_state_.at(3) = sqrt(pow(twist_.linear.x, 2) +
       pow(twist_.linear.y, 2));
 
   //std::cout<<"==================="<<std::endl;
@@ -163,13 +170,13 @@ void RL::TaskEnvIO::getRobotState(){
 
 /// use tf to call the robot pose and speed DEPRECATED too slow
 float RL::TaskEnvIO::getRobotStateTF(){
-  std::clock_t c_start = std::clock(); 
+  std::clock_t c_start = std::clock();
   tf::StampedTransform transform_;
   try{
     tf_listener.lookupTransform("base_link",
         "target_pose",
         ros::Time(0),
-        transform_); 
+        transform_);
   }
   catch (tf::TransformException ex){
     ROS_ERROR("%s",ex.what());
@@ -181,20 +188,20 @@ float RL::TaskEnvIO::getRobotStateTF(){
   float angle_ = atan2(transform_.getOrigin().y(),
       transform_.getOrigin().x());
 
-  float distance_ = sqrt(pow(transform_.getOrigin().x(), 2) + 
+  float distance_ = sqrt(pow(transform_.getOrigin().x(), 2) +
       pow(transform_.getOrigin().y(), 2));
 
   std::cout<<"angle: "<<angle_<<std::endl;
   std::cout<<"distance: "<<distance_<<std::endl;
 
-  std::clock_t c_start_2 = std::clock(); 
+  std::clock_t c_start_2 = std::clock();
   geometry_msgs::Twist twsit_;
   try{
     tf_listener.lookupTwist("default_world",
         "base_link",
         ros::Time(0),
         ros::Duration(0.01),
-        twsit_ ); 
+        twsit_ );
   }
   catch (tf::TransformException ex){
     ROS_ERROR("%s",ex.what());
@@ -205,7 +212,7 @@ float RL::TaskEnvIO::getRobotStateTF(){
 
   float angle_vel_ = twsit_.angular.z;
 
-  float lin_vel_ = sqrt(pow(twsit_.linear.x, 2) + 
+  float lin_vel_ = sqrt(pow(twsit_.linear.x, 2) +
       pow(twsit_.linear.y, 2));
 
   std::cout<<"angular vel:  "<<angle_vel_<<std::endl;
@@ -213,7 +220,3 @@ float RL::TaskEnvIO::getRobotStateTF(){
 
   return 0;
 }
-
-
-
-
