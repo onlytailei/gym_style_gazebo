c84f912eb8f73030bb488079deb5067ee5145a1a
Lei Tai docker <onlytailei@hotmail.com>
2017-08-31 07:50:58 +0000

    sleep for longer time

--- src/task_env_io.cpp
+++ src/task_env_io.cpp
@@ -117,7 +117,7 @@ bool RL::TaskEnvIO::ServiceCallback(
       ROS_ERROR("Reset loop");
       this->reset();
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
   
   if (!req.reset){
